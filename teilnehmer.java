cc77321ae16db2e0bd5125033fac1cd894b3436d
gomaaz <gomaaz@gmx.de>
2016-03-08 14:48:39 +0100

    wichtiges Update Cool

--- teilnehmer.java
+++ teilnehmer.java
@@ -1,6 +1,7 @@
 
 public class teilnehmer {
   public static void main(String[] args) {
-    System.out.println("Ne, ernsthaft jetzt: Ich lerne Java!");
-    // https://www.youtube.com/playlist?list=PL0AE7270FED49C567
+    System.out.println("ich");
+    System.out.print(" bin");
+    System.out.println("cool");
   }
