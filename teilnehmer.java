de7047d9bffd63b0f225bd9f78b422936ee2c328
gomaaz <gomaaz@gmx.de>
2016-03-08 09:13:07 +0100

    update

--- teilnehmer.java
+++ teilnehmer.java
@@ -1,3 +1,5 @@
-public static void main(String[] args) {
-  System.out.println("Ne, ernsthaft jetzt: Ich lerne Java!");
+public class teilnehmer {
+  public static void main(String[] args) {
+    System.out.println("Ne, ernsthaft jetzt: Ich lerne Java!");
+  }
 }
