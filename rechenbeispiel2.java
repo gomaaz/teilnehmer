487b10ff64cfcc8bd6765122415ceb347bd78c8b
gomaaz <gomaaz@gmx.de>
2016-03-08 22:27:03 +0100

    Updates zu rechenbeispielen

--- rechenbeispiel2.java
+++ rechenbeispiel2.java
@@ -1,5 +1,5 @@
 
-public class teilnehmer {
+public class rechenbeispiel2 {
   public static void main(String[] args) {
 
     int a = 3;
@@ -10,12 +10,12 @@ public class teilnehmer {
     int int_ergebnis;
     double double_ergebnis;
 
-    System.out.println("b + c * 6 = " + b + c * 6); // 536 Text
-    // System.out.println("b - c * 6 = " + b - c * 6);
-    // System.out.println("(x * c - a) = " + (x * c - a));
-    // System.out.println("x + c * 6 = " + x + c * 6);
-    // System.out.println("y - c / a = " + (y - c / a));
-    // System.out.println("b + a * x + y = " + b + a * x + y);
+    System.out.println("b + c * 6 = " + b + c * 6); // 536 Textverkettung b mit gerechneten 6*6
+    //System.out.println("b - c * 6 = " + b - c * 6); // zeichenkette "b - C * 6 = 5" wird mit Int wert verknüpft-> compilerfehler
+    System.out.println("(x * c - a) = " + (x * c - a)); // 6.0 -> 1,5*6-3
+    System.out.println("x + c * 6 = " + x + c * 6); // "1,5"als String + 36 als string
+    System.out.println("y - c / a = " + (y - c / a)); // 0.3
+    System.out.println("b + a * x + y = " + b + a * x + y); // 54.52.3 Textverkettungen
     // System.out.println("b + a * x * y = " + b + a * x * y);
     // System.out.println("b + a * x - y = " + b + a * x - y);
 
