6876dfea420c974f78529be468dc7e8574b47023
gomaaz <gomaaz@gmx.de>
2016-03-08 13:55:59 +0100

    feature B

--- teilnehmer.java
+++ teilnehmer.java
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
+
 public class teilnehmer {
   public static void main(String[] args) {
     System.out.println("Ne, ernsthaft jetzt: Ich lerne Java!");
   }
-=======
-public static void main(String[] args) {
-  System.out.println("Nee, Du ist doof");
->>>>>>> 10fadc549a9cd5302678d778dd6e7eb0b08565ae
-}
+
 //hallo
 
-///testedstge
+///testedstge dies ist feature B
