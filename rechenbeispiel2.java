59db1dfe843bbb2f8ad505eebd063fb8ca51d0a4
gomaaz <gomaaz@gmx.de>
2016-03-08 22:09:24 +0100

    rechenbeispiel

---
+++ rechenbeispiel2.java
@@ -0,0 +1,25 @@
+
+public class teilnehmer {
+  public static void main(String[] args) {
+
+    int a = 3;
+    int b = 5;
+    int c = 6;
+    double x = 1.5;
+    double y = 2.3;
+    int int_ergebnis;
+    double double_ergebnis;
+
+    System.out.println("b + c * 6 = " + b + c * 6); // 536 Text
+    // System.out.println("b - c * 6 = " + b - c * 6);
+    // System.out.println("(x * c - a) = " + (x * c - a));
+    // System.out.println("x + c * 6 = " + x + c * 6);
+    // System.out.println("y - c / a = " + (y - c / a));
+    // System.out.println("b + a * x + y = " + b + a * x + y);
+    // System.out.println("b + a * x * y = " + b + a * x * y);
+    // System.out.println("b + a * x - y = " + b + a * x - y);
+
+  }
+}
+//dsdsd
+//new kommentar
